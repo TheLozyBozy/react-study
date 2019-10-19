a1cd9ff54dd81baa11647ceaa64aeb1c6756f5d4
ajarrar <ajarar.jo@gmail.com>
2019-10-19 20:36:48 +0300

    05 - Loading CSS into our React Application

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -5,12 +5,14 @@ class StorePicker extends React.Component {
         return(
            // This to allow us to make a multi element in the same level 
            <React.Fragment> 
-               {
+               
+               {/* This is for JS CODE */
                    console.log("sad") /* Dont add a comment like this // 
                    or add a comment in empty like this 
                    {/* this is a comment }*/
                 }
                <form action="" className="store-selector">
+                   <h2>Please Enter A Store</h2>
                     <input type="text" required placeholder="Store Name" />
                     <button type="submit">Visit Store</button>
                 </form> 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
+import './css/style.css';
 import React  from 'react'; 
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
-render(<StorePicker />, document.getElementById("main"))
+render(<StorePicker />, document.getElementById("main"));
