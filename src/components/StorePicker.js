53489464a59c76e5cc3c239c1a488337ddb5b2d2
ajarrar <ajarar.jo@gmail.com>
2019-10-19 20:23:35 +0300

    4 - Writing HTML with JSX

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,20 @@ import React  from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <form action="" className="store-selector">
-            
-        </form> ;
+        return(
+           // This to allow us to make a multi element in the same level 
+           <React.Fragment> 
+               {
+                   console.log("sad") /* Dont add a comment like this // 
+                   or add a comment in empty like this 
+                   {/* this is a comment }*/
+                }
+               <form action="" className="store-selector">
+                    <input type="text" required placeholder="Store Name" />
+                    <button type="submit">Visit Store</button>
+                </form> 
+           </React.Fragment> 
+        ); 
     }
 }
 
