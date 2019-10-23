a2ccacf41cb3d994b8c7568d8409857431668f9b
ajarrar <ajarar.jo@gmail.com>
2019-10-23 21:26:40 +0300

    08 - Stateless Functional Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,19 @@
 import React from 'react';
-
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-               <h1>Carch 
-                   <span className="ofThe">
-                        <span className="of">of</span>
-                        <span className="the">the</span>
-                   </span> Day</h1> 
-               <h3 className="tagline">
-                    <span>{this.props.tagline}</span>
-               </h3>
-            </header>
-        )
-    }
-}
+/*  
+    you destructing  the probs when you using { }
+    or you can use props only 
+*/
+const Header = ({tagline , age , checked}) =>  (
+    <header className="top">
+        <h1>Carch 
+            <span className="ofThe">
+                <span className="of">of</span>
+                <span className="the">the</span>
+            </span> Day</h1> 
+        <h3 className="tagline">
+            <span>{tagline + age + checked} </span>
+        </h3>
+    </header>
+)
 
 export default Header;
