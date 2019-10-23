bc96f41de1b798a14fd028eed0dad0c13076cdf4
Ahmad <ajarar@atypon.com>
2019-10-23 14:21:18 +0300

    07 - Passing Dynamic data with props

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,13 @@ import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
 class App extends React.Component {
+     /* If you wanna pass string use "" if number use {} */
     render() {
         return (
             <React.Fragment>
                 <div className="catch-of-the-day">
                     <div className="menu">
-                         <Header />
+                         <Header tagline="Catch-of-the-day" age={500} />
                     </div>
                     <Inventory/>
                     <Order/>
