fb15c5355035c39de112eb66a29f0a30b1aa269b
Ahmad <ajarar@atypon.com>
2019-10-28 11:19:01 +0200

    10 - Helper and Utility Functions

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,5 +1,5 @@
 import React  from 'react'; 
-
+import { getFunName } from "../helpers"
 class StorePicker extends React.Component {
     render() {
         return(
@@ -13,7 +13,7 @@ class StorePicker extends React.Component {
                 }
                <form action="" className="store-selector">
                    <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" />
+                    <input type="text" required placeholder="Store Name" value={getFunName()}/>
                     <button type="submit">Visit Store</button>
                 </form> 
            </React.Fragment> 
