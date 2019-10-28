0401ce5c0067f1a5b58f9de27cb1e6c3a7b568db
Ahmad <ajarar@atypon.com>
2019-10-28 12:46:35 +0200

    11 - Events, Refs and this Binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,6 +1,34 @@
 import React  from 'react'; 
 import { getFunName } from "../helpers"
 class StorePicker extends React.Component {
+    
+    myInput = React.createRef();
+    /* You can chose this answer but if you alot of method you will face an ugly constructor */
+    /* Use the arrow function will resolve this problem */
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this)
+    // }
+    // goToStore(event) {
+
+    //     //1: stop submitting 
+    //     event.preventDefault();
+
+    //     //2: get the text from the input 
+    //     console.log(this.myInput)
+
+    //     //3: change the path 
+    // }
+
+
+    goToStore = event => {
+        //1: stop submitting 
+        event.preventDefault();
+
+        //2: get the text from the input 
+        console.log(this.myInput)
+        //3: change the path 
+    }
     render() {
         return(
            // This to allow us to make a multi element in the same level 
@@ -10,11 +38,12 @@ class StorePicker extends React.Component {
                    console.log("sad") /* Dont add a comment like this // 
                    or add a comment in empty like this 
                    {/* this is a comment }*/
+                   // if we add () after functtion -> after this the function will run auto 
                 }
-               <form action="" className="store-selector">
+               <form action="" className="store-selector" onSubmit={this.goToStore}>
                    <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" value={getFunName()}/>
-                    <button type="submit">Visit Store</button>
+                    <input ref={this.myInput} type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                    <button type="submit">Visit Store</button> 
                 </form> 
            </React.Fragment> 
         ); 
